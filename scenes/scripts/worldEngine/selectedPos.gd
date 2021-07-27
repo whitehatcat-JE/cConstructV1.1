8551570d7a874592e9df9c2588e7aebd8a5138c0
Jayden <everesja@gmail.com>
2021-07-28 00:38:36 +1200

    Started working on fixing grid for placing objects

--- scenes/scripts/worldEngine/selectedPos.gd
+++ scenes/scripts/worldEngine/selectedPos.gd
@@ -1,7 +1,6 @@
 extends Spatial
 
 var curAim = Vector3()
-var objOffset = 0.01
 
 # Terrain Selector
 func updateAim(newAim):
@@ -43,7 +42,7 @@ func rotateGridCursor(clockwise:bool=true):
 
 # Scales the grid
 func scaleGrid(size):
-	$gridOverlay.scale = Vector3(size + objOffset, size + objOffset, size + objOffset)
+	$gridOverlay.scale = Vector3(size, size, size)
 # Adjust rotation of Flora
 func rotateSpray():
 	$floraDisplay.rotate_y(deg2rad(90))
