abc8f52e0cd247b87fe106ebf6c1d27661be543b
cattythegamer-tcat <55483594+cattythegamer-tcat@users.noreply.github.com>
2021-08-10 09:24:24 +1200

    Connected item selection signals to itemManager

---
+++ scenes/scripts/worldEngine/itemManager/itemCollectionInterface.gd
@@ -0,0 +1,104 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_item_button_down():
+	pass # Replace with function body.
+
+
+func _on_item2_button_down():
+	pass # Replace with function body.
+
+
+func _on_item3_button_down():
+	pass # Replace with function body.
+
+
+func _on_item4_button_down():
+	pass # Replace with function body.
+
+
+func _on_item5_button_down():
+	pass # Replace with function body.
+
+
+func _on_item6_button_down():
+	pass # Replace with function body.
+
+
+func _on_item7_button_down():
+	pass # Replace with function body.
+
+
+func _on_item8_button_down():
+	pass # Replace with function body.
+
+
+func _on_item9_button_down():
+	pass # Replace with function body.
+
+
+func _on_item10_button_down():
+	pass # Replace with function body.
+
+
+func _on_item11_button_down():
+	pass # Replace with function body.
+
+
+func _on_item12_button_down():
+	pass # Replace with function body.
+
+
+func _on_item13_button_down():
+	pass # Replace with function body.
+
+
+func _on_item14_button_down():
+	pass # Replace with function body.
+
+
+func _on_item15_button_down():
+	pass # Replace with function body.
+
+
+func _on_item16_button_down():
+	pass # Replace with function body.
+
+
+func _on_item17_button_down():
+	pass # Replace with function body.
+
+
+func _on_item18_button_down():
+	pass # Replace with function body.
+
+
+func _on_item19_button_down():
+	pass # Replace with function body.
+
+
+func _on_item20_button_down():
+	pass # Replace with function body.
+
+
+func _on_item21_button_down():
+	pass # Replace with function body.
+
+
+func _on_item22_button_down():
+	pass # Replace with function body.
